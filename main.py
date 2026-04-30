a307e97fbf21565c163fc7de3428e87f18b27450
Juaum-zim <aedo.joao@tutamail.com>
2026-04-29 22:30:54 -0300

    Página inicial restaurada

--- main.py
+++ main.py
@@ -24,17 +24,17 @@ async def main(page: ft.Page):
         page.views.clear()
         page.views.append(
             # Página Base
-            chatView(page.width, page.height)
-            # ft.View(
-            #     route="/",
-            #     controls=[
-            #         ft.Text("Tela Base", size=32),
-            #         ft.Button(content="Login", on_click=open_login),
-            #         ft.Button(content="Chat", on_click=open_chat),
-            #     ],
-            #     vertical_alignment=ft.MainAxisAlignment.CENTER,
-            #     horizontal_alignment=ft.CrossAxisAlignment.CENTER,
-            # )
+            #chatView(page.width, page.height)
+            ft.View(
+                route="/",
+                controls=[
+                    ft.Text("Tela Base", size=32),
+                    ft.Button(content="Login", on_click=open_login),
+                    ft.Button(content="Chat", on_click=open_chat),
+                ],
+                vertical_alignment=ft.MainAxisAlignment.CENTER,
+                horizontal_alignment=ft.CrossAxisAlignment.CENTER,
+            )
         )
 
         if page.route == "/login":
