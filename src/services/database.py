f92e98af858eca28b96095fcafa3a45a7054bfce
Arthur-Zanardi <arthurmzanardi@gmail.com>
2026-04-30 11:26:27 -0300

    'correcao bug'

--- src/services/database.py
+++ src/services/database.py
@@ -82,10 +82,17 @@ def popular_banco_mock():
     if len(resultados['ids']) == 0:
         print("Populando banco com perfis de teste...")
         
-        # Use aqueles mesmos dicionários de teste (perfil_maria, perfil_carmen, etc)
-        perfil_maria = { ... } # (Cole aqui o JSON da Maria que usamos antes)
-        perfil_carmen = { ... } # (Cole aqui o JSON da Carmen que usamos antes)
-        
+        perfil_maria = {
+            "psicologico": {"extroversao": 0.6, "abertura_experiencias": 0.6, "romantismo_afeto": 0.5, "ritmo_vida": 0.6, "logica_vs_emocao": 0.7, "resolucao_conflitos": 0.5, "competitividade_cooperacao": 0.5},
+            "valores": {"ambicao_carreira": 0.6, "conservadorismo": 0.4, "espectro_politico": 0.5, "gestao_financeira": 0.6, "religiosidade": 0.4, "gosto_festas": 0.6},
+            "interesses": {"animes": 0.9, "filmes": 0.6, "series": 0.7, "livros_ficcao": 0.8, "videogames": 0.7, "jogos_tabuleiro": 0.4, "tecnologia": 0.6, "academia": 0.8, "esportes": 0.8, "futebol": 0.9, "dancas": 0.3, "musica": 0.6, "tocar_instrumentos": 0.2, "fotografia": 0.4, "culinaria": 0.6, "idiomas": 0.6, "celebridades": 0.2, "historia": 0.5, "geografia": 0.4, "geopolitica": 0.5, "astronomia": 0.5}
+        }
+
+        perfil_carmen = {
+            "psicologico": {"extroversao": 0.2, "abertura_experiencias": 0.2, "romantismo_afeto": 0.8, "ritmo_vida": 0.2, "logica_vs_emocao": 0.2, "resolucao_conflitos": 0.2, "competitividade_cooperacao": 0.2},
+            "valores": {"ambicao_carreira": 0.2, "conservadorismo": 1.0, "espectro_politico": 0.9, "gestao_financeira": 0.2, "religiosidade": 1.0, "gosto_festas": 0.0},
+            "interesses": {"animes": 0.0, "filmes": 0.2, "series": 0.2, "livros_ficcao": 0.1, "videogames": 0.0, "jogos_tabuleiro": 0.1, "tecnologia": 0.1, "academia": 0.0, "esportes": 0.0, "futebol": 0.0, "dancas": 0.1, "musica": 0.5, "tocar_instrumentos": 0.0, "fotografia": 0.1, "culinaria": 0.8, "idiomas": 0.1, "celebridades": 0.8, "historia": 0.2, "geografia": 0.2, "geopolitica": 0.1, "astronomia": 0.0}
+        }
         salvar_perfil_usuario("user_maria", "Maria", perfil_maria)
         salvar_perfil_usuario("user_carmen", "Carmen", perfil_carmen)
         print("Perfis de teste criados!")
